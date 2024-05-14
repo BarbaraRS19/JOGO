ae3e998a1476701945c9196e1bc9ab5eaf1870b5
BarbaraRS19 <barbara_sabino@estudante.sesisenai.org.br>
2024-05-14 10:46:40 -0300

    asd

--- matriz.js
+++ matriz.js
@@ -1,4 +1,7 @@
-let button,quebraLinha, jogada = 1
+//tornar os if menores, 1 para linha, 1 para coluna, 1 para diagonais, de forma dinâmica!!!
+//criar um botão para reiniciar o jogo
+//sei la2.9
+let button,quebraLinha, jogada = 1, vencedor = 0
 let tabuleiro = new Array(3)
 for(let i = 0; i < tabuleiro.length; i++){
     tabuleiro[i] = new Array(3)
@@ -12,46 +15,77 @@ for(let i = 0; i < tabuleiro.length; i++){
     button.setAttribute('type', 'button')
     button.setAttribute('id','bt' + i + "" + j)
     button.setAttribute('class', 'btJogo' + i)
-    button.setAttribute('onclick', 'marca' + i + "" + j +"()")
+    button.setAttribute('onclick', 'marca(' + i + "," + j +")")
     button.append(document.createTextNode(""))
     document.body.append(button)
     }
 }
 
-function marca00(){
-    marcaCasa("bt00")
-}
-function marca01(){
-    marcaCasa("bt01")
-}
-function marca02(){
-    marcaCasa("bt02")
-}
-function marca10(){
-    marcaCasa("bt10")
-}
-function marca11(){
-    marcaCasa("bt11")
-}
-function marca12(){
-    marcaCasa("bt12")
-}
-function marca20(){
-    marcaCasa("bt20")
-}
-function marca21(){
-    marcaCasa("bt21")
-}
-function marca22(){
-    marcaCasa("bt22")
+function marca(linha, coluna){
+    marcaCasa("bt" + linha + "" + coluna)
 }
 
 function marcaCasa(nomeBotao){
 if(jogada % 2 == 0){
     document.getElementById(nomeBotao).innerText = "O"
+    document.getElementById(nomeBotao).style.color =  "blue"
 }else{
     document.getElementById(nomeBotao).innerText = "X"
+    document.getElementById(nomeBotao).style.color =  "red"
 }
+document.getElementById(nomeBotao).disabled = true
+let line = nomeBotao.charAt(2)
+let column = nomeBotao.charAt(3)
 jogada++
+if(jogada >= 5){
+    encerraJogo()
+}
+if(jogada >= 9){
+alert("Jogo Empatado!")
+}
+}
+
+function encerraJogo(){
+if(document.getElementById("bt00").innerText == document.getElementById("bt01").innerText 
+&& document.getElementById("bt01").innerText == document.getElementById("bt02").innerText
+&& document.getElementById("bt00").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt00").innerText)
+vencedor ++
+}
+if(document.getElementById("bt10").innerText == document.getElementById("bt11").innerText 
+&& document.getElementById("bt11").innerText == document.getElementById("bt12").innerText
+&& document.getElementById("bt10").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt10").innerText)
+}
+if(document.getElementById("bt20").innerText == document.getElementById("bt21").innerText 
+&& document.getElementById("bt21").innerText == document.getElementById("bt22").innerText
+&& document.getElementById("bt20").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt20").innerText)
+}
+if(document.getElementById("bt00").innerText == document.getElementById("bt11").innerText 
+&& document.getElementById("bt11").innerText == document.getElementById("bt22").innerText
+&& document.getElementById("bt00").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt00").innerText)
+}
+if(document.getElementById("bt02").innerText == document.getElementById("bt11").innerText 
+&& document.getElementById("bt11").innerText == document.getElementById("bt20").innerText
+&& document.getElementById("bt02").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt02").innerText)
+}
+if(document.getElementById("bt00").innerText == document.getElementById("bt10").innerText 
+&& document.getElementById("bt10").innerText == document.getElementById("bt20").innerText
+&& document.getElementById("bt00").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt00").innerText)
+}
+if(document.getElementById("bt01").innerText == document.getElementById("bt11").innerText 
+&& document.getElementById("bt11").innerText == document.getElementById("bt21").innerText
+&& document.getElementById("bt01").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt01").innerText)
+}
+if(document.getElementById("bt02").innerText == document.getElementById("bt12").innerText 
+&& document.getElementById("bt12").innerText == document.getElementById("bt22").innerText
+&& document.getElementById("bt02").innerText != ""){
+alert("Jogo Finalizado! \n Vencedor: " + document.getElementById("bt02").innerText)
+}
 }
 
